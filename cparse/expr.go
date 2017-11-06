73235971ca9d9e4578b50f6f4cb94a4f1c464653
Simon Schmidt <simon.schmidt@htp-tel.de>
2017-11-06 13:34:22 +0100

    Syntax-tree priorization

--- cparse/expr.go
+++ cparse/expr.go
@@ -116,13 +116,14 @@ func c_expr_cast(p *parser.Parser,tokens *scanlist.Element, left interface{}) pa
 	
 	e,t := parser.Match(parser.Textify,tp.Next,/*(*/')')
 	if e!=nil { return parser.ResultFail(fmt.Sprint(e),tp.Next.SafePos()) }
-	sub := p.MatchNoLeftRecursion("Expr",t)
+	sub := p.MatchNoLeftRecursion("Expr2",t)
 	if sub.Result==parser.RESULT_OK {
 		sub.Data = &Expr{E_CAST,"cast",aR(tp.Data,sub.Data),tokens.Pos}
 	}
 	return sub
 }
-func c_expr(p *parser.Parser,tokens *scanlist.Element, left interface{}) parser.ParserResult {
+
+func c_expr0(p *parser.Parser,tokens *scanlist.Element, left interface{}) parser.ParserResult {
 	if tokens==nil { return parser.ResultFail("EOF!",scanner.Position{}) }
 	switch tokens.Token {
 	case scanner.Ident: return parser.ResultOk(tokens.Next(),&Expr{E_VAR,tokens.TokenText,nil,tokens.Pos})
@@ -131,7 +132,7 @@ func c_expr(p *parser.Parser,tokens *scanlist.Element, left interface{}) parser.
 	case scanner.Char: return parser.ResultOk(tokens.Next(),&Expr{E_CHAR,tokens.TokenText,nil,tokens.Pos})
 	case scanner.String,scanner.RawString: return parser.ResultOk(tokens.Next(),&Expr{E_STRING,tokens.TokenText,nil,tokens.Pos})
 	case '*','+','-','!','~','&':{
-		sub := p.MatchNoLeftRecursion("Expr",tokens.Next())
+		sub := p.MatchNoLeftRecursion("Expr0",tokens.Next())
 		if sub.Result==parser.RESULT_OK {
 			sub.Data = &Expr{E_UNARY_OP,tokens.TokenText,aR(sub.Data),tokens.Pos}
 		}
@@ -149,7 +150,7 @@ func c_expr(p *parser.Parser,tokens *scanlist.Element, left interface{}) parser.
 	}
 	return parser.ResultFail("Invalid Expression!",tokens.Pos)
 }
-func c_expr_trailer(p *parser.Parser,tokens *scanlist.Element, left interface{}) parser.ParserResult {
+func c_expr_trailer0(p *parser.Parser,tokens *scanlist.Element, left interface{}) parser.ParserResult {
 	if ok,t := parser.FastMatch(tokens,'+','+'); ok {
 		return parser.ResultOk(t,&Expr{E_INCR,"++",aR(left),tokens.Pos})
 	}
@@ -183,28 +184,85 @@ func c_expr_trailer(p *parser.Parser,tokens *scanlist.Element, left interface{})
 		}
 		return sub
 	}
-	if tokens.SafeToken()=='?' {
-		sub := p.Match("Expr",tokens.Next())
-		if sub.Result!=parser.RESULT_OK { return sub }
-		e,t := parser.Match(parser.Textify,sub.Next,':')
-		if e!=nil { return parser.ResultFail(fmt.Sprint(e),sub.Next.SafePos()) }
-		sub2 := p.MatchNoLeftRecursion("Expr",t)
-		if sub2.Result!=parser.RESULT_OK { return sub2 }
-		return parser.ResultOk(sub2.Next,&Expr{E_CONDITIONAL,"?:",aR(left,sub.Data,sub2.Data),tokens.Pos})
+	return parser.ResultFail("No trailer.",tokens.SafePos())
+}
+
+func c_expr1(p *parser.Parser,tokens *scanlist.Element, left interface{}) parser.ParserResult {
+	if tokens==nil { return parser.ResultFail("EOF!",scanner.Position{}) }
+	switch tokens.Token {
+	case '*','+','-','!','~','&':{
+		sub := p.MatchNoLeftRecursion("Expr1",tokens.Next())
+		if sub.Result==parser.RESULT_OK {
+			sub.Data = &Expr{E_UNARY_OP,tokens.TokenText,aR(sub.Data),tokens.Pos}
+		}
+		return sub
+	    }
 	}
-	
-	
-	ok,t := parser.FastMatch(tokens,'+')
-	s := ""
-	if !ok { ok,t = parser.FastMatch(tokens,'-') }
-	if !ok { ok,t = parser.FastMatch(tokens,'*') }
+	return p.Match("Expr0",tokens)
+}
+func c_expr2(p *parser.Parser,tokens *scanlist.Element, left interface{}) parser.ParserResult {
+	return p.Match("Expr1",tokens)
+}
+func c_expr3(p *parser.Parser,tokens *scanlist.Element, left interface{}) parser.ParserResult {
+	return p.Match("Expr2",tokens)
+}
+func c_expr_trailer3(p *parser.Parser,tokens *scanlist.Element, left interface{}) parser.ParserResult {
+	ok,t := parser.FastMatch(tokens,'*')
 	if !ok { ok,t = parser.FastMatch(tokens,'/') }
 	if !ok { ok,t = parser.FastMatch(tokens,'%') }
+	
+	if ok {
+		s := tokens.TokenText
+		op := E_BINARY_OP
+		if t.SafeToken()=='=' {
+			op = E_BINARY_OP_ASSIGN
+			t = t.SafeNext()
+		}
+		sub := p.MatchNoLeftRecursion("Expr3",t)
+		if sub.Result==parser.RESULT_OK {
+			sub.Data = &Expr{op,s,aR(left,sub.Data),tokens.Pos}
+		}
+		return sub
+	}
+	return parser.ResultFail("No trailer.",tokens.SafePos())
+}
+
+func c_expr4(p *parser.Parser,tokens *scanlist.Element, left interface{}) parser.ParserResult {
+	return p.Match("Expr3",tokens)
+}
+func c_expr_trailer4(p *parser.Parser,tokens *scanlist.Element, left interface{}) parser.ParserResult {
+	ok,t := parser.FastMatch(tokens,'+')
+	if !ok { ok,t = parser.FastMatch(tokens,'-') }
+	
+	if ok {
+		s := tokens.TokenText
+		op := E_BINARY_OP
+		if t.SafeToken()=='=' {
+			op = E_BINARY_OP_ASSIGN
+			t = t.SafeNext()
+		}
+		sub := p.MatchNoLeftRecursion("Expr4",t)
+		if sub.Result==parser.RESULT_OK {
+			sub.Data = &Expr{op,s,aR(left,sub.Data),tokens.Pos}
+		}
+		return sub
+	}
+	return parser.ResultFail("No trailer.",tokens.SafePos())
+}
+
+func c_expr5(p *parser.Parser,tokens *scanlist.Element, left interface{}) parser.ParserResult {
+	return p.Match("Expr4",tokens)
+}
+func c_expr_trailer5(p *parser.Parser,tokens *scanlist.Element, left interface{}) parser.ParserResult {
+	var ok bool
+	var t  *scanlist.Element
+	ok = false
+	s := ""
 	if !ok { ok,t = parser.FastMatch(tokens,'>','>'); if ok { s=">>" } }
 	if !ok { ok,t = parser.FastMatch(tokens,'<','<'); if ok { s="<<" } }
-	if !ok { ok,t = parser.FastMatch(tokens,'&','&'); if ok { s="&&" } }
-	if !ok { ok,t = parser.FastMatch(tokens,'|','|'); if ok { s="||" } }
 	if !ok { ok,t = parser.FastMatch(tokens,'^') }
+	if !ok { ok,t = parser.FastMatch(tokens,'|') }
+	if !ok { ok,t = parser.FastMatch(tokens,'&') }
 	
 	if ok {
 		if s=="" { s = tokens.TokenText }
@@ -213,33 +271,82 @@ func c_expr_trailer(p *parser.Parser,tokens *scanlist.Element, left interface{})
 			op = E_BINARY_OP_ASSIGN
 			t = t.SafeNext()
 		}
-		sub := p.MatchNoLeftRecursion("Expr",t)
+		sub := p.MatchNoLeftRecursion("Expr5",t)
 		if sub.Result==parser.RESULT_OK {
 			sub.Data = &Expr{op,s,aR(left,sub.Data),tokens.Pos}
 		}
 		return sub
-		/*
-		if sub.Result==parser.RESULT_OK {
-			sub.Data = &Expr{E_BINARY_OP,tokens.TokenText,aR(left,sub.Data)}
-		}
-		*/
 	}
-	
-	ok,t = parser.FastMatch(tokens,'=','='); if ok { s="==" }
+	return parser.ResultFail("No trailer.",tokens.SafePos())
+}
+func c_expr6(p *parser.Parser,tokens *scanlist.Element, left interface{}) parser.ParserResult {
+	return p.Match("Expr5",tokens)
+}
+func c_expr_trailer6(p *parser.Parser,tokens *scanlist.Element, left interface{}) parser.ParserResult {
+	s := ""
+	ok,t := parser.FastMatch(tokens,'=','='); if ok { s="==" }
 	if !ok { ok,t = parser.FastMatch(tokens,'!','='); if ok { s="!=" } }
 	if !ok { ok,t = parser.FastMatch(tokens,'<','='); if ok { s="<=" } }
 	if !ok { ok,t = parser.FastMatch(tokens,'<'); if ok { s="<" } }
 	if !ok { ok,t = parser.FastMatch(tokens,'>','='); if ok { s=">=" } }
 	if !ok { ok,t = parser.FastMatch(tokens,'>'); if ok { s=">" } }
 	if ok {
-		sub := p.MatchNoLeftRecursion("Expr",t)
+		sub := p.MatchNoLeftRecursion("Expr6",t)
 		if sub.Result==parser.RESULT_OK {
 			sub.Data = &Expr{E_COMPARE,s,aR(left,sub.Data),tokens.Pos}
 		}
 		return sub
 	}
+	return parser.ResultFail("No trailer.",tokens.SafePos())
+}
+
+func c_expr7(p *parser.Parser,tokens *scanlist.Element, left interface{}) parser.ParserResult {
+	return p.Match("Expr6",tokens)
+}
+func c_expr_trailer7(p *parser.Parser,tokens *scanlist.Element, left interface{}) parser.ParserResult {
+	var t *scanlist.Element
+	ok := false
+	s := ""
+	if !ok { ok,t = parser.FastMatch(tokens,'&','&'); if ok { s="&&" } }
+	if !ok { ok,t = parser.FastMatch(tokens,'|','|'); if ok { s="||" } }
 	
-	ok,t = parser.FastMatch(tokens,'=')
+	if ok {
+		if s=="" { s = tokens.TokenText }
+		op := E_BINARY_OP
+		if t.SafeToken()=='=' {
+			op = E_BINARY_OP_ASSIGN
+			t = t.SafeNext()
+		}
+		sub := p.MatchNoLeftRecursion("Expr7",t)
+		if sub.Result==parser.RESULT_OK {
+			sub.Data = &Expr{op,s,aR(left,sub.Data),tokens.Pos}
+		}
+		return sub
+	}
+	return parser.ResultFail("No trailer.",tokens.SafePos())
+}
+
+func c_expr8(p *parser.Parser,tokens *scanlist.Element, left interface{}) parser.ParserResult {
+	return p.Match("Expr7",tokens)
+}
+func c_expr_trailer8(p *parser.Parser,tokens *scanlist.Element, left interface{}) parser.ParserResult {
+	if tokens.SafeToken()=='?' {
+		sub := p.Match("Expr7",tokens.Next())
+		if sub.Result!=parser.RESULT_OK { return sub }
+		e,t := parser.Match(parser.Textify,sub.Next,':')
+		if e!=nil { return parser.ResultFail(fmt.Sprint(e),sub.Next.SafePos()) }
+		sub2 := p.MatchNoLeftRecursion("Expr7",t)
+		if sub2.Result!=parser.RESULT_OK { return sub2 }
+		return parser.ResultOk(sub2.Next,&Expr{E_CONDITIONAL,"?:",aR(left,sub.Data,sub2.Data),tokens.Pos})
+	}
+	return parser.ResultFail("No trailer.",tokens.SafePos())
+}
+
+func c_expr(p *parser.Parser,tokens *scanlist.Element, left interface{}) parser.ParserResult {
+	return p.Match("Expr8",tokens)
+}
+func c_expr_trailer(p *parser.Parser,tokens *scanlist.Element, left interface{}) parser.ParserResult {
+	ok,t := parser.FastMatch(tokens,'=')
 	
 	if ok {
 		sub := p.Match("Expr",t)
@@ -248,11 +355,42 @@ func c_expr_trailer(p *parser.Parser,tokens *scanlist.Element, left interface{})
 		}
 		return sub
 	}
-		
+	
 	return parser.ResultFail("No trailer.",tokens.SafePos())
 }
 
+/*
+The following expression classes are registered.
+	'Expr0' // Primary Expression + Suffix
+	'Expr1' // Unary Expression
+	'Expr2' // Cast Expression
+	'Expr3' // Multiplicative Expression
+	'Expr4' // Additive Expression
+	'Expr5' // Bitwise expression
+	'Expr6' // Relational expression
+	'Expr7' // Logical expression
+	'Expr8' // Conditional expression
+	'Expr'  // Expression
+*/
 func RegisterExpr(p *parser.Parser) {
+	p.Define("Expr0",false,parser.Pfunc(c_expr0))
+	p.Define("Expr0",true,parser.Pfunc(c_expr_trailer0))
+	
+	p.Define("Expr1",false,parser.Pfunc(c_expr1))
+	p.Define("Expr2",false,parser.Pfunc(c_expr2))
+	p.Define("Expr3",false,parser.Pfunc(c_expr3))
+	p.Define("Expr3",true,parser.Pfunc(c_expr_trailer3))
+	p.Define("Expr4",false,parser.Pfunc(c_expr4))
+	p.Define("Expr4",true,parser.Pfunc(c_expr_trailer4))
+	p.Define("Expr5",false,parser.Pfunc(c_expr5))
+	p.Define("Expr5",true,parser.Pfunc(c_expr_trailer5))
+	p.Define("Expr6",false,parser.Pfunc(c_expr6))
+	p.Define("Expr6",true,parser.Pfunc(c_expr_trailer6))
+	p.Define("Expr7",false,parser.Pfunc(c_expr7))
+	p.Define("Expr7",true,parser.Pfunc(c_expr_trailer7))
+	p.Define("Expr8",false,parser.Pfunc(c_expr8))
+	p.Define("Expr8",true,parser.Pfunc(c_expr_trailer8))
+	
 	p.Define("Expr",false,parser.Pfunc(c_expr))
 	p.Define("Expr",true,parser.Pfunc(c_expr_trailer))
 }
@@ -263,6 +401,6 @@ Adds the casting operation (Type)Expr.
 */
 func RegisterExprCast(p *parser.Parser) {
 	p.TouchRule("Type")
-	p.DefineBefore("Expr",false,parser.Pfunc(c_expr_cast))
+	p.DefineBefore("Expr2",false,parser.Pfunc(c_expr_cast))
 }
 
